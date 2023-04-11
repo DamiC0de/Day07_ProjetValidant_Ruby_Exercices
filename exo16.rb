3f9e7fca1021d2257b6b7e43cf6c62b8d74bb0af
DamiC0de <dreymidot@gmail.com>
2023-04-11 11:04:02 +0200

    script make a pyramid.rb program that will go up instead of down

---
+++ exo16.rb
@@ -0,0 +1,14 @@
+# Demande un nombre entre 1 et 25 à l'utilisateur
+puts "Veuillez entrer un nombre entre 1 et 25 :"
+nombre = gets.chomp.to_i
+
+# Vérifie si le nombre est bien entre 1 et 25
+if nombre < 1 || nombre > 25
+  puts "Le nombre doit être compris entre 1 et 25."
+else
+  # Affiche la pyramide qui monte
+  (1..nombre).each do |i|
+    espaces = nombre - i
+    puts " " * espaces + "#" * i
+  end
+end
