9de2c3e0563a71662039090c3296d44bad36d159
DamiC0de <dreymidot@gmail.com>
2023-04-12 11:05:19 +0200

    modified exo14.rb & exo15.rb, add comments

--- exo14.rb
+++ exo14.rb
@@ -12,3 +12,18 @@ faux_emails.each do |email|
     puts email
   end
 end
+
+
+# Le code initialise un tableau vide appelé faux_emails avec faux_emails = [].
+
+# Il entre ensuite dans une boucle each pour les nombres de 1 à 50 (inclus) : (1..50).each do |i|.
+
+# Pour chaque itération de cette boucle, la variable i prend une valeur allant de 1 à 50. Le code construit alors un faux e-mail avec la syntaxe suivante : email = "jean.dupont.%02d@email.fr" % i.
+
+# La chaîne de caractères "jean.dupont.%02d@email.fr" contient un "format spécificateur" : %02d. Ce format spécificateur indique que la chaîne doit inclure un entier (ici, la valeur de i) sur 2 chiffres, en ajoutant éventuellement un zéro devant pour compléter.
+# L'opérateur % est utilisé pour remplacer le format spécificateur %02d par la valeur de i. Par exemple, si i est égal à 3, la chaîne deviendra "jean.dupont.03@email.fr".
+# Une fois l'e-mail créé, il est ajouté à la fin du tableau faux_emails avec faux_emails << email.
+
+# La boucle each se termine après avoir généré 50 faux e-mails.
+
+# À la fin de l'exécution de ce code, la variable faux_emails contient un tableau de 50 faux e-mails sous la forme "jean.dupont.01@email.fr", "jean.dupont.02@email.fr", etc.
--- exo15.rb
+++ exo15.rb
@@ -14,3 +14,19 @@ end while nombre < 1 || nombre > 25
 (1..nombre).each do |i|
   puts "#" * i
 end
+
+
+# clamp -> permet d'aller récupérer la borne autorisé entre deux chiffres .clamp(1,25)
+
+# Ce code Ruby demande à l'utilisateur d'entrer un nombre entre 1 et 25 et affiche ensuite une pyramide descendante basée sur ce nombre. Voici une explication détaillée du code :
+
+# Le code commence par une boucle begin..end while qui s'exécute tant que l'utilisateur n'a pas entré un nombre valide compris entre 1 et 25.
+# Le programme affiche un message demandant à l'utilisateur d'entrer un nombre entre 1 et 25 : puts "Veuillez entrer un nombre entre 1 et 25 :".
+# Il récupère ensuite l'entrée de l'utilisateur avec gets.chomp.to_i. gets permet de récupérer l'entrée de l'utilisateur, chomp retire le saut de ligne à la fin de l'entrée, et to_i convertit l'entrée en un entier. Le nombre est stocké dans la variable nombre.
+# Le code vérifie si le nombre entré est bien compris entre 1 et 25 avec la condition if nombre < 1 || nombre > 25. Si le nombre n'est pas valide, le programme affiche un message d'erreur : puts "Le nombre doit être compris entre 1 et 25.".
+# La boucle begin..end while continue de s'exécuter tant que le nombre entré par l'utilisateur n'est pas compris entre 1 et 25 (nombre < 1 || nombre > 25).
+# Une fois que l'utilisateur a entré un nombre valide :
+
+# Le code entre dans une autre boucle, cette fois-ci une boucle each pour les nombres de 1 à nombre (inclus) : (1..nombre).each do |i|.
+# Pour chaque itération de cette boucle, la variable i prend une valeur allant de 1 à nombre. Le code affiche alors un certain nombre de caractères dièse (#) correspondant à la valeur de i : puts "#" * i.
+# Ainsi, en exécutant ce code, l'utilisateur obtient une pyramide descendante composée de caractères dièse (#), dont la base contient autant de dièses que le nombre entré par l'utilisateur.
